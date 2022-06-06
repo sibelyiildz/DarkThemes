be8a2cc99a21768f4ba3b2ade1f5ef1230cb9c1a
Sibel Yıldız <sibelyldz2012@gmail.com>
2022-06-06 11:13:38 +0300

    themes change feature added

--- app/src/main/java/com/sibelyiildz/darkmodesample/MainActivity.kt
+++ app/src/main/java/com/sibelyiildz/darkmodesample/MainActivity.kt
@@ -1,14 +1,61 @@
 package com.sibelyiildz.darkmodesample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val SELECTED_THEME = "selected_theme"
+    }
+
+    private lateinit var sharedPRef: SharedPreferences
+    private lateinit var prefEditor: SharedPreferences.Editor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sharedPRef = getSharedPreferences(packageName, Context.MODE_PRIVATE)
+        prefEditor = sharedPRef.edit()
 
-        Log.v("LogTag", "activity çalıştı")
+        val currentSelectedTheme = sharedPRef.getInt(SELECTED_THEME, AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        AppCompatDelegate.setDefaultNightMode(currentSelectedTheme)
+
+        when (currentSelectedTheme) {
+            AppCompatDelegate.MODE_NIGHT_NO -> {
+                findViewById<RadioButton>(R.id.light).isChecked = true
+            }
+            AppCompatDelegate.MODE_NIGHT_YES -> {
+                findViewById<RadioButton>(R.id.night).isChecked = true
+            }
+            AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM -> {
+                findViewById<RadioButton>(R.id.systemDefault).isChecked = true
+            }
+        }
+
+        findViewById<RadioGroup>(R.id.radioGroup).setOnCheckedChangeListener { _, radioButton ->
+            when (radioButton) {
+                findViewById<RadioButton>(R.id.light).id -> {
+                    setThemes(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+                findViewById<RadioButton>(R.id.night).id -> {
+                    setThemes(AppCompatDelegate.MODE_NIGHT_YES)
+                }
+                findViewById<RadioButton>(R.id.systemDefault).id -> {
+                    setThemes(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                }
+            }
+        }
+    }
+
+    private fun setThemes(theme: Int) {
+        prefEditor.putInt(SELECTED_THEME, theme)
+        AppCompatDelegate.setDefaultNightMode(theme)
+        prefEditor.apply()
     }
 }
